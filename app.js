979f449157531c6fadb1a74650b914259210e948
OwaisZakir <owaiszakir88@gmial.com>
2024-11-09 19:07:59 +0500

    update code

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-var countDownDate = new Date("January 01, 2024 00:00:00");
+let countDownDate = new Date("January 01, 2025 00:00:00");
+let currentDate = new Date();
+
+let days = Math.floor((countDownDate - currentDate) / (1000 * 60 * 60 * 24));
+let hours = Math.floor((countDownDate - currentDate) / (1000 * 60 * 60));
+let minutes = Math.floor((countDownDate - currentDate) / (1000 * 60));
+let seconds = Math.floor((countDownDate - currentDate) / 1000);
