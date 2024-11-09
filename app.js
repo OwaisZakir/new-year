2b8d7eb748c104fdca61eb5ce1c7aa05856cc067
OwaisZakir <owaiszakir88@gmial.com>
2024-11-09 20:09:33 +0500

    function Done

--- app.js
+++ app.js
@@ -1,7 +1,31 @@
+// ================ Dom Elements ===========
+let domDays = document.querySelector("#days");
+let domHours = document.querySelector("#hours");
+let domMinutes = document.querySelector("#minutes");
+let domSeconds = document.querySelector("#Seconds");
+// ================== Date Variables ==============
 let countDownDate = new Date("January 01, 2025 00:00:00");
-let currentDate = new Date();
+// let currentDate = new Date();
+// let tootleDuration = countDownDate - currentDate;
+// let days = Math.floor(tootleDuration / (1000 * 60 * 60 * 24));
+// let hours = Math.floor(
+//   (tootleDuration % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+// );
+// let minutes = Math.floor((tootleDuration % (1000 * 60 * 60)) / (1000 * 60));
+// let seconds = Math.floor((tootleDuration % (1000 * 60)) / 1000);
 
-let days = Math.floor((countDownDate - currentDate) / (1000 * 60 * 60 * 24));
-let hours = Math.floor((countDownDate - currentDate) / (1000 * 60 * 60));
-let minutes = Math.floor((countDownDate - currentDate) / (1000 * 60));
-let seconds = Math.floor((countDownDate - currentDate) / 1000);
+setInterval(function () {
+  let currentDate = new Date();
+  let tootleDuration = countDownDate - currentDate;
+  let days = Math.floor(tootleDuration / (1000 * 60 * 60 * 24));
+  let hours = Math.floor(
+    (tootleDuration % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  let minutes = Math.floor((tootleDuration % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((tootleDuration % (1000 * 60)) / 1000);
+
+  domDays.innerHTML = days;
+  domHours.innerHTML = hours;
+  domMinutes.innerHTML = minutes;
+  domSeconds.innerHTML = seconds;
+}, 1000);
