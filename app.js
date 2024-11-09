8f5fb230a060633b7c5edac7b1e8d532852ce85a
OwaisZakir <owaiszakir88@gmial.com>
2024-11-09 20:37:37 +0500

    functionally Done

--- app.js
+++ app.js
@@ -1,20 +1,14 @@
 // ================ Dom Elements ===========
-let domDays = document.querySelector("#days");
-let domHours = document.querySelector("#hours");
-let domMinutes = document.querySelector("#minutes");
-let domSeconds = document.querySelector("#Seconds");
+let domCounterDays = document.querySelector("#days");
+let domCounterHours = document.querySelector("#hours");
+let domCounterMinutes = document.querySelector("#minutes");
+let domCounterSeconds = document.querySelector("#Seconds");
 // ================== Date Variables ==============
 let countDownDate = new Date("January 01, 2025 00:00:00");
-// let currentDate = new Date();
-// let tootleDuration = countDownDate - currentDate;
-// let days = Math.floor(tootleDuration / (1000 * 60 * 60 * 24));
-// let hours = Math.floor(
-//   (tootleDuration % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-// );
-// let minutes = Math.floor((tootleDuration % (1000 * 60 * 60)) / (1000 * 60));
-// let seconds = Math.floor((tootleDuration % (1000 * 60)) / 1000);
-
-setInterval(function () {
+let counter = countDownDate;
+let current = new Date();
+// =================== Set Interval ===================
+let running = setInterval(function () {
   let currentDate = new Date();
   let tootleDuration = countDownDate - currentDate;
   let days = Math.floor(tootleDuration / (1000 * 60 * 60 * 24));
@@ -24,8 +18,23 @@ setInterval(function () {
   let minutes = Math.floor((tootleDuration % (1000 * 60 * 60)) / (1000 * 60));
   let seconds = Math.floor((tootleDuration % (1000 * 60)) / 1000);
 
-  domDays.innerHTML = days;
-  domHours.innerHTML = hours;
-  domMinutes.innerHTML = minutes;
-  domSeconds.innerHTML = seconds;
+  domCounterDays.innerHTML = days;
+  domCounterHours.innerHTML = hours;
+  domCounterMinutes.innerHTML = minutes;
+  domCounterSeconds.innerHTML = seconds;
 }, 1000);
+// ================= Validations ===========================
+if (current < counter) {
+  document.getElementById("day").innerHTML = "Days";
+  document.getElementById("hour").innerHTML = "Hours";
+  document.getElementById("minute").innerHTML = "Minutes";
+  document.getElementById("second").innerHTML = "Seconds";
+
+  domCounterDays.innerHTML = "0";
+  domCounterHours.innerHTML = "0";
+  domCounterMinutes.innerHTML = "0";
+  domCounterSeconds.innerHTML = "0";
+} else {
+  clearInterval(running);
+  document.getElementById("timerClose").innerHTML = "Happy New Year 2025!";
+}
